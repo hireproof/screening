704fc69061f2320d3df3b7a73461b765b0f16768
Niklas Klein <mail@taig.io>
2022-03-20 16:05:35 +0100

    Fix dependabot format

--- project/DependabotGenerator.scala
+++ project/DependabotGenerator.scala
@@ -4,11 +4,13 @@ import io.circe.syntax._
 object DependabotGenerator {
   def apply(): Json = Json.obj(
     "version" := 2,
-    "updates" := Json.obj(
-      "package-ecosystem" := "github-actions",
-      "directory" := "/",
-      "schedule" := Json.obj(
-        "interval" := "daily"
+    "updates" := List(
+      Json.obj(
+        "package-ecosystem" := "github-actions",
+        "directory" := "/",
+        "schedule" := Json.obj(
+          "interval" := "daily"
+        )
       )
     )
   )
